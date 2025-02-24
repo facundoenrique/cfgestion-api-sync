79dbd4caca7425c3f4456e79e2486077e7bd5fdc
Facundo Enrique <52930089+facundoenrique@users.noreply.github.com>
2025-02-24 20:41:42 -0300

    first commit

---
+++ src/main/java/org/api_sync/ApiSyncApplication.java
@@ -0,0 +1,13 @@
+package org.api_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/api_sync/ApiSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

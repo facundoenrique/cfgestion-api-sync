cdb1ec747ffa51432128fe9cf269903d1f3bf5ff
Facundo Enrique <52930089+facundoenrique@users.noreply.github.com>
2025-02-25 21:37:26 -0300

    add Articulos

--- src/main/java/org/api_sync/domain/Articulos.java
+++ src/main/java/org/api_sync/domain/Articulos.java
@@ -1,45 +1,74 @@
 package org.api_sync.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+
 import java.sql.Date;
 
 @Entity
 @Table(name = "articulos")
-@Data
+@Getter
 public class Articulos {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @Column @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date fecha_creado; //es una fecha, despues lo tengo que analizar bien para que se usa.
+    @Column @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCreado; //es una fecha, despues lo tengo que analizar bien para que se usa.
+    @Column
     private int enviado;
+    @Column
     private double precio1;
+@Column
     private int familia;
+@Column
     private int tipo;
+@Column
     private double descuento;
+@Column
     private float iva;
+@Column
     private String nombre;
+@Column
     private String descripcion;
-    private int cod_unidad_medida;
+@Column
+    private int codUnidadMedida;
+@Column
     private double costo;
+@Column
     private int minimo;
+@Column
     private String numero; // este es el codigo de barras que se ingresa en la facturacion, tiene que ser String
+@Column
     private int empresa;
-    private int no_stock;
+@Column
+    private int noStock;
+@Column
     private int moneda;
+@Column
     private int proveedor;
+@Column
     private int defecto;//cantidad de venta, cada vez que se pasa el codigo de barras, por lo gral es uno, pero puede haber casos en que no.
+@Column
     private int eliminado;
+@Column
     private double gan1;
+@Column
     private int maximo;
-    private int defecto_compra;
+@Column
+    private int defectoCompra;
+@Column
     private int comision;
+@Column
     private int redondeo;
+@Column
     private int imagen;
+@Column
     private int subfamilia;
+@Column
     private int compuesto;
+@Column
     private String marca;
-    private int cod_iva;
+@Column
+    private int codIva;
+@Column
     private double cantidad;
 }

7a9809178e3759893b3fb6f6d9728e178a187087
Facundo Enrique <52930089+facundoenrique@users.noreply.github.com>
2025-04-06 16:01:43 -0300

    fix abm clientes

--- src/main/java/org/api_sync/services/clientes/mappers/ClienteMapper.java
+++ src/main/java/org/api_sync/services/clientes/mappers/ClienteMapper.java
@@ -2,7 +2,9 @@ package org.api_sync.services.clientes.mappers;
 
 import org.api_sync.adapter.inbound.request.ClienteRequest;
 import org.api_sync.adapter.outbound.entities.Cliente;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ClienteMapper {
 	public Cliente toEntity(ClienteRequest request) {
 		return Cliente.builder()

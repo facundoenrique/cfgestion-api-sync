93a01fa374af9cc3b7c9f3b84d1e467c4a487d2c
Facundo Enrique <52930089+facundoenrique@users.noreply.github.com>
2025-04-16 23:52:51 -0300

    get preventas

--- src/main/java/org/api_sync/adapter/inbound/PropuestaController.java
+++ src/main/java/org/api_sync/adapter/inbound/PropuestaController.java
@@ -6,15 +6,19 @@ import org.api_sync.adapter.inbound.request.PropuestaRequestDTO;
 import org.api_sync.adapter.inbound.responses.PropuestaResponseDTO;
 import org.api_sync.adapter.outbound.entities.Propuesta;
 import org.api_sync.adapter.outbound.entities.PropuestaArticulo;
+import org.api_sync.services.lista_precios.dto.ListaPreciosDTO;
 import org.api_sync.services.proposals.PropuestaService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -24,6 +28,27 @@ public class PropuestaController {
 
 	private final PropuestaService propuestaService;
 
+	@GetMapping
+	public ResponseEntity<Page<PropuestaResponseDTO>> findAll(
+			@RequestParam(required = false) LocalDate fechaDesde,
+			@RequestParam(required = false) LocalDate fechaHasta,
+			@RequestParam(required = false) Long proveedorId,
+			@RequestParam(required = false) String nombre,
+			@PageableDefault(size = 10, sort = "fechaCreacion", direction = Sort.Direction.DESC) Pageable pageable
+	) {
+		return ResponseEntity.ok(propuestaService.listar(fechaDesde, fechaHasta, proveedorId,
+				nombre, pageable));
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<PropuestaResponseDTO> findById(@PathVariable Long id) {
+		Optional<Propuesta> propuesta = propuestaService.getListaPrecio(id);
+		if (propuesta.isPresent()) {
+			return ResponseEntity.ok(new PropuestaResponseDTO(propuesta.get()));
+		}
+		return ResponseEntity.noContent().build();
+	}
+	
 	@PostMapping
 	public ResponseEntity<PropuestaResponseDTO> crearPropuesta(@Valid @RequestBody PropuestaRequestDTO requestDTO) {
 		Propuesta propuesta = new Propuesta();
--- src/main/java/org/api_sync/services/proposals/PropuestaService.java
+++ src/main/java/org/api_sync/services/proposals/PropuestaService.java
@@ -1,8 +1,16 @@
 package org.api_sync.services.proposals;
 
+import io.micrometer.common.util.StringUtils;
+import java.time.LocalDate;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
+import org.api_sync.adapter.inbound.responses.PropuestaResponseDTO;
 import org.api_sync.adapter.outbound.entities.Propuesta;
 import org.api_sync.adapter.outbound.repository.PropuestaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +19,38 @@ public class PropuestaService {
 
 	private final PropuestaRepository propuestaRepository;
 
+	public Optional<Propuesta> getListaPrecio(Long id) {
+		Optional<Propuesta> propuesta = propuestaRepository.findById(id);
+		if (propuesta.isPresent()) {
+			return propuesta;
+		}
+		return Optional.empty();
+	}
+	
+	public Page<PropuestaResponseDTO> listar(LocalDate fechaDesde,
+	                                         LocalDate fechaHasta,
+	                                         Long proveedorId,
+	                                         String nombre,
+	                                         Pageable pageable) {
+		Specification<Propuesta> spec = Specification.where(null);
+		
+		if (fechaDesde != null) {
+			spec = spec.and((root, query, cb) -> cb.greaterThanOrEqualTo(root.get("fechaCreacion"), fechaDesde));
+		}
+		if (fechaHasta != null) {
+			spec = spec.and((root, query, cb) -> cb.lessThanOrEqualTo(root.get("fechaCreacion"), fechaHasta));
+		}
+		if (proveedorId != null) {
+			spec = spec.and((root, query, cb) -> cb.equal(root.get("proveedor").get("id"), proveedorId));
+		}
+		if (StringUtils.isNotBlank(nombre)) {
+			spec = spec.and((root, query, cb) -> cb.like(cb.lower(root.get("nombre")), "%" + nombre.toLowerCase() + "%"));
+		}
+		
+		Page<Propuesta> propuestaPage = propuestaRepository.findAll(spec, pageable);
+		
+		return propuestaPage.map(PropuestaResponseDTO::new);
+	}
 	
 	public Propuesta guardarPropuesta(Propuesta propuesta) {
 		return propuestaRepository.save(propuesta);

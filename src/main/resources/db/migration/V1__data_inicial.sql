9bf6d2bd0e078a6a0f87479059bd87253f9cc481
Facundo Enrique <52930089+facundoenrique@users.noreply.github.com>
2025-05-26 23:39:12 -0300

    Update V1__data_inicial.sql

--- src/main/resources/db/migration/V1__data_inicial.sql
+++ src/main/resources/db/migration/V1__data_inicial.sql
@@ -901,11 +901,26 @@ ALTER TABLE `proveedores`
 ALTER TABLE comprobantes ADD CONSTRAINT unique_numero_empresa UNIQUE (numero, empresa_id);
 ALTER TABLE comprobantes ADD CONSTRAINT unique_codigo_empresa UNIQUE (codigo, empresa_id);
 
+
 --
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `FK9v93lqnass5yqhhsyprr9fdv2` FOREIGN KEY (`empresa_id`) REFERENCES `empresas` (`id`);
+
+-- Insertar empresa Super Ideal
+INSERT INTO empresas (uuid, nombre, cuit, razon_social, condicion_iva)
+VALUES ('6d2fd451-d36c-44c5-bae2-22a6d6b7d094', 'Supermercado Ideal', '27166080091', 'Caffaro Miriam Leticia', 1);
+
+-- Insertar empresa La Nueva Estrella
+INSERT INTO empresas (uuid, nombre, cuit, razon_social, condicion_iva)
+VALUES ('da7fd6f4-1d81-4f33-b848-b7be5bca4752', 'La Nueva Estrella', '20252155857', 'Dube Ariel Alejandro', 1);
+
+-- Insertar usuario facu (password: admin123)
+INSERT INTO usuarios (nombre, password, empresa_id, eliminado, codigo)
+VALUES ('gabriel', 'kEAQfs/kL1UxPIYMw71FhBA+wfSmKsVoZ+mKiENrmmU=', 1, 0, 1);
+
+
 COMMIT;
 
 

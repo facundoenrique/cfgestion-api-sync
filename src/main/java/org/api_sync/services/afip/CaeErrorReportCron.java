c62043ec2f52792bf2bb4c9cb710f89d63d2dd28
Facundo Enrique <52930089+facundoenrique@users.noreply.github.com>
2025-07-09 13:03:07 -0300

    fix destinatario email

--- src/main/java/org/api_sync/services/afip/CaeErrorReportCron.java
+++ src/main/java/org/api_sync/services/afip/CaeErrorReportCron.java
@@ -1,19 +1,22 @@
 package org.api_sync.services.afip;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class CaeErrorReportCron {
 
     private final CaeErrorMemory caeErrorMemory;
     private final MailService mailService;
-
-    public CaeErrorReportCron(CaeErrorMemory caeErrorMemory, MailService mailService) {
-        this.caeErrorMemory = caeErrorMemory;
-        this.mailService = mailService;
-    }
+    @Value("${cfgestion.mail.main-mail}")
+    private String emailMain;
 
     // Ejecuta todos los días a las 11:30 y 19:30
     @Scheduled(cron = "0 30 11,19 * * *")
@@ -24,21 +27,16 @@ public class CaeErrorReportCron {
         String body = errores.stream()
             .map(e -> "Punto de venta: " + e.getPuntoVenta() + ", Tipo: " + e.getTipoTraducido() + ", Error: " + e.getErrorMessage())
             .collect(Collectors.joining("<br/>"));
-
-        String destinatario = getDestinatarioFromDbMock();
-
+        
         try {
+            String destinatario = StringUtils.isNotBlank(emailMain) ? emailMain : "facuenrique@gmail.com";
+            log.info("Enviando email con body :{}", body);
             mailService.sendMail(destinatario, "Errores CAE del día", body);
             // Limpiar los errores después de enviar el mail
             errores.forEach(e -> caeErrorMemory.clearError(e.getPuntoVenta(), e.getTipo()));
         } catch (Exception ex) {
-            // Manejo de error de envío de mail
-            ex.printStackTrace();
+            log.error(ex.getMessage(), ex);
         }
     }
 
-    // Mock para obtener el destinatario desde la base de datos
-    private String getDestinatarioFromDbMock() {
-        return "destinatario@dominio.com";
-    }
 } 
